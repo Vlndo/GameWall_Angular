92adc9da24d04b4c8c61a201cefbd18f66a57bcb
Vlndo <ernaudpiels@icloud.com>
2023-09-18 12:40:37 +0200

    a3-gestion-du-token-jwt-guard

--- src/app/_helpers/auth.guard.ts
+++ src/app/_helpers/auth.guard.ts
@@ -1,34 +1,38 @@
-import { CanActivateFn } from "@angular/router";
+// import { CanActivateFn } from "@angular/router";
 
-export const AuthGuard: CanActivateFn = (route, state) => {
-	return true;
-};
+// export const AuthGuard: CanActivateFn = (route, state) => {
+// 	constructor(private router: Router){}
+
+// };
 
 //------------------------ VERSION DETAILLEE --------------------------------
 
-// import { CanActivateFn } from "@angular/router";
-// import { Observable } from "rxjs";
-// import {
-// 	ActivatedRouteSnapshot,
-// 	CanActivate,
-// 	Router,
-// 	RouterStateSnapshot,
-// 	UrlTree,
-// } from "@angular/router";
-// import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import {
+	ActivatedRouteSnapshot,
+	CanActivate,
+	Router,
+	RouterStateSnapshot,
+	UrlTree,
+} from "@angular/router";
+import { Injectable } from "@angular/core";
+import { TokenService } from "../_services/token.service";
 
-// @Injectable({
-// 	providedIn: "root",
-// })
-// export class AuthGuard implements CanActivate {
-// 	canActivate(
-// 		route: ActivatedRouteSnapshot,
-// 		state: RouterStateSnapshot,
-// 	):
-// 		| Observable<boolean | UrlTree>
-// 		| Promise<boolean | UrlTree>
-// 		| boolean
-// 		| UrlTree {
-// 		return false;
-// 	}
-// }
+@Injectable({
+	providedIn: "root",
+})
+export class AuthGuard implements CanActivate {
+	constructor(private router: Router, private token: TokenService) {}
+
+	canActivate(
+		route: ActivatedRouteSnapshot,
+		state: RouterStateSnapshot,
+	):
+		| Observable<boolean | UrlTree>
+		| Promise<boolean | UrlTree>
+		| boolean
+		| UrlTree {
+		if (this.token.isLogged()) return true;
+		else return this.router.navigate(["auth"]);
+	}
+}
--- src/app/admin/aheader/aheader.component.ts
+++ src/app/admin/aheader/aheader.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { TokenService } from "src/app/_services/token.service";
 
 @Component({
-  selector: 'app-aheader',
-  templateUrl: './aheader.component.html',
-  styleUrls: ['./aheader.component.css']
+	selector: "app-aheader",
+	templateUrl: "./aheader.component.html",
+	styleUrls: ["./aheader.component.css"],
 })
 export class AheaderComponent {
+	constructor(private token: TokenService) {}
 
+	logOut(): void {
+		this.token.clearToken();
+	}
 }
