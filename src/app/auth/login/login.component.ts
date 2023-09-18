f96d085ceacab6919d6f4bd50b5ed42105380e62
Vlndo <ernaudpiels@icloud.com>
2023-09-18 10:52:51 +0200

    a2-creation-formulaire-de-connexion

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -16,7 +16,14 @@ export class LoginComponent {
 
 	onSubmit(): void {
 		this.auth.login(this.form).subscribe(
+<<<<<<< Updated upstream
 			(data) => console.log(data.token),
+=======
+			(data) => {
+				console.log(data.token);
+				localStorage.setItem("token", data.token);
+			},
+>>>>>>> Stashed changes
 			(err) => console.log(err),
 		);
 	}
