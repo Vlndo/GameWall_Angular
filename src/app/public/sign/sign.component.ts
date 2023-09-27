ff6571d99d617f7497a9c9379ece02661def629b
Vlndo <ernaudpiels@icloud.com>
2023-09-27 10:49:31 +0200

    inscription sans le country

--- src/app/public/sign/sign.component.ts
+++ src/app/public/sign/sign.component.ts
@@ -15,14 +15,26 @@ export class SignComponent {
 		age: "",
 		isadmin: false,
 		image: "image",
+		// country: "",
 	};
+	countries: any[] = [];
 	url: string = "http://127.0.0.1:8000/api/users";
+	urlGet: string = "http://127.0.0.1:8000/api/countries";
 
 	constructor(private http: HttpClient, private router: Router) {}
 
+	ngOnInit() {
+		this.http.get(this.urlGet).subscribe((response: any) => {
+			this.countries = response["hydra:member"];
+			console.log(this.countries);
+		});
+	}
+
 	onSubmit() {
+		// console.log(this.user);
 		this.http.post(this.url, this.user).subscribe((data) => {
 			console.log("vos données ont été envoyées");
+			// console.log(data);
 		});
 		return this.router.navigate(["auth"]);
 	}
