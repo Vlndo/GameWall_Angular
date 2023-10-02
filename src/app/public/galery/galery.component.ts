40bceb8c936c889b745371eb1c981fe804aba7ba
azuera <terminatur45@gmail.com>
2023-10-02 15:51:44 +0200

    a7-filtres

--- src/app/public/galery/galery.component.ts
+++ src/app/public/galery/galery.component.ts
@@ -1,19 +1,64 @@
 import { Component } from "@angular/core";
 import { ProductsService } from "src/app/_services/products.service";
+
 @Component({
-	selector: "app-galery",
-	templateUrl: "./galery.component.html",
-	styleUrls: ["./galery.component.css"],
+  selector: "app-galery",
+  templateUrl: "./galery.component.html",
+  styleUrls: ["./galery.component.css"],
 })
 export class GaleryComponent {
-	products: any[] = [];
+  products: any[] = [];
+  searchQuery: string = '';
+  filteredProducts: any[] = [];
+  selectedPlatform: string = '';
+  uniquePlatforms: string[] = [];
+  uniqueTags: string[] = [];
+  selectedTag: string = '';
+  constructor(private productsService: ProductsService) {}
 
-	constructor(private productsService: ProductsService) {}
+  ngOnInit() {
+    this.productsService.getProducts().subscribe((response) => {
+      this.products = response["hydra:member"];
+      this.filteredProducts = this.products;
+      this.uniquePlatforms = this.getUniquePlatforms(this.products);
+      this.uniqueTags = this.getUniqueTags(this.products);
 
-	ngOnInit() {
-		this.productsService.getProducts().subscribe((response) => {
-			this.products = response["hydra:member"];
-			console.log(this.products);
-		});
-	}
+      console.log(this.products);
+    });
+  }
+
+  filterProducts() {
+    this.filteredProducts = this.products.filter(product =>
+      (product.title.toLowerCase().includes(this.searchQuery.toLowerCase()) || this.searchQuery === '') &&
+      (this.selectedPlatform === '' || this.productHasPlatform(product, this.selectedPlatform)) &&
+      (this.selectedTag === '' || this.productHasTag(product, this.selectedTag))
+    );
+  }
+
+  productHasPlatform(product: any, platformName: string): boolean {
+    return product.platforms.some((platform: { name: string; }) => platform.name === platformName);
+  }
+
+
+  getUniquePlatforms(products: any[]): string[] {
+    const platformsSet = new Set<string>();
+    products.forEach(product => {
+      product.platforms.forEach((platform: { name: string; }) => {
+        platformsSet.add(platform.name);
+      });
+    });
+    return Array.from(platformsSet);
+  }
+  productHasTag(product: any, tagName: string): boolean {
+    return product.tags.some((tag: { name: string; }) => tag.name === tagName);
+  }
+  getUniqueTags(products: any[]): string[] {
+    const tagsSet = new Set<string>();
+    products.forEach(product => {
+      product.tags.forEach((tag: { name: string; }) => {
+        tagsSet.add(tag.name);
+      });
+    });
+    return Array.from(tagsSet);
+  }
 }
