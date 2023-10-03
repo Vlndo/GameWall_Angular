6b682e42da4901c0d91cbf207baff9fb047974a3
Vlndo <ernaudpiels@icloud.com>
2023-10-03 17:03:50 +0200

    affichage des images et du prix du produit dans le panier

--- src/app/public/paiment/paiment.module.ts
+++ src/app/public/paiment/paiment.module.ts
@@ -1,10 +1,10 @@
 import { PaimentRoutingModule } from "./paiment-routing.module";
 import { CartComponent } from "./cart/cart.component";
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { CommonModule, NgOptimizedImage } from "@angular/common";
 
 @NgModule({
 	declarations: [CartComponent],
-	imports: [PaimentRoutingModule, CommonModule],
+	imports: [PaimentRoutingModule, CommonModule, NgOptimizedImage],
 })
 export class PaimentModule {}
